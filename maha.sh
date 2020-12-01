aa8d7ab1d37f262713a07d497cb72b2baa644a6f
rockybhai0516 <42719351+rockybhai0516@users.noreply.github.com>
2020-12-01 14:14:17 +0530

    Update maha.sh

--- maha.sh
+++ maha.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-dir = /root/Recon
 
 if [ ! -d "$1" ]; then
         mkdir -p $dir/$1
 fi
 
-amass enum -passive -norecursive -noalts -d $1 -o $dir/$1/$1.txt
-cat $dir/$1/$1.txt | httpx -o $dir/$1/$1httpx.txt
-cat $dir/$1/$1httpx.txt | nuclei -t nuclei-templates/ -o $dir/$1/nucli.txt
-rm -fr $dir/$1/$1.txt
+amass enum -passive -norecursive -noalts -d $1 -o /root/Recon/$1/$1.txt
+cat /root/Recon/$1/$1.txt | httpx -o $dir/$1/$1httpx.txt
+cat /root/Recon/$1/$1httpx.txt | nuclei -t nuclei-templates/ -o /root/Recon/$1/nucli.txt
+rm -fr /root/Recon/$1/$1.txt
