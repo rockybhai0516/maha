ab2690f74e5e1e40c9e3c67bf3365f25809d929c
rockybhai0516 <42719351+rockybhai0516@users.noreply.github.com>
2020-12-01 14:20:18 +0530

    Update maha.sh

--- maha.sh
+++ maha.sh
@@ -7,5 +7,5 @@ fi
 
 amass enum -passive -norecursive -noalts -d $1 -o /root/Recon/$1/$1.txt
 cat /root/Recon/$1/$1.txt | httpx -o /root/Recon/$1/$1httpx.txt
-cat /root/Recon/$1/$1httpx.txt | nuclei -t nuclei-templates/ -o /root/Recon/$1/nucli.txt
+cat /root/Recon/$1/$1httpx.txt | nuclei -t nuclei-templates/ 
 rm -fr /root/Recon/$1/$1.txt
