02104206dea37008aa8482c65b236daf49b663bc
rockybhai0516 <42719351+rockybhai0516@users.noreply.github.com>
2020-12-01 14:17:32 +0530

    Update maha.sh

--- maha.sh
+++ maha.sh
@@ -6,6 +6,6 @@ if [ ! -d "$1" ]; then
 fi
 
 amass enum -passive -norecursive -noalts -d $1 -o /root/Recon/$1/$1.txt
-cat /root/Recon/$1/$1.txt | httpx -o $dir/$1/$1httpx.txt
+cat /root/Recon/$1/$1.txt | httpx -o /root/Recon/$1/$1httpx.txt
 cat /root/Recon/$1/$1httpx.txt | nuclei -t nuclei-templates/ -o /root/Recon/$1/nucli.txt
 rm -fr /root/Recon/$1/$1.txt
